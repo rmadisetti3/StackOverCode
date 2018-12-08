914f97a9e34182380e25174bd73648081e8b1275
pomyo <yeddev@gmail.com>
2018-12-08 10:26:26 -0500

    pull changes

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,6 +1,13 @@
+/**
+ * 
+ */
 
 const editor = {
     currentEditor: null,
+    /**
+     * 
+     * @param {CodeMirror: Object} _currentEditor 
+     */
     create: (container, language) => {
         editor.destroy(container);
 
@@ -22,14 +29,26 @@ const editor = {
 
         editor.listeners.onCursorActivity(editor.currentEditor);
     },
+    /**
+     * 
+     * @param {CodeMirror: Object} _currentEditor 
+     */
     destroy: (container) => {
         $(container).empty();
     },
+    /**
+     * 
+     * @param {CodeMirror: Object} _currentEditor 
+     */
     hasAQuestion: (line) => {
         if(line.includes("//? ")) {
             return line.substring(line.indexOf("//? "), line.length).length > 4;
         };
     },
+    /**
+     * 
+     * @param {CodeMirror: Object} _currentEditor 
+     */
     scanForQuestions: () => {
         let questions = [];
         editor.currentEditor.doc.eachLine( (line) => { 
@@ -39,6 +58,10 @@ const editor = {
         });
         return questions;
     },
+    /**
+     * 
+     * @param {CodeMirror: Object} _currentEditor 
+     */
     addReplaceAllFunc: () => {
         String.prototype.replaceAll = function(search, replacement) {
             var target = this;
@@ -46,12 +69,24 @@ const editor = {
         };
     },
     listeners: {
+        /**
+         * 
+         * @param {CodeMirror: Object} _currentEditor 
+         */
         selectEditorLanguage:   () => {
             $(".select-items").children().click( () => { 
                 editor.create($("#editor"), 
                 $(".same-as-selected")[0].textContent);
             })
         },
+        /**
+         * debounce function to delay api calls
+         * @version https://github.com/johndugan/javascript-debounce
+         * @author John Dugan
+         * @param {Function} func 
+         * @param {Milliseconds} wait 
+         * @param {Boolean} immediate 
+         */
         debounce: function(func, wait, immediate) {
             var timeout;
             return function() {
@@ -71,6 +106,10 @@ const editor = {
                 }
             };
         },
+        /**
+         * 
+         * @param {CodeMirror: Object} _currentEditor 
+         */
         onCursorActivity: (_currentEditor) => {
             _currentEditor.on('cursorActivity', editor.listeners.debounce(
                 () => {
@@ -82,6 +121,10 @@ const editor = {
                 }, 5000, false))
         }
     },
+     /**
+     *
+     * @param {CodeMirror: Object} _currentEditor 
+     */
     init: () => {
         editor.listeners.selectEditorLanguage();
         editor.addReplaceAllFunc();
